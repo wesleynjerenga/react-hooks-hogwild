b9edd3344db5c42bd5ab5bf008cbdac444522ac2
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-12 09:04:49 +0300

    hogCard.js

---
+++ src/components/HogCard.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function HogCard({ hog }) {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleClick = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div className="ui card" onClick={handleClick}>
+      <div className="image">
+        <img src={hog.image} alt={hog.name} />
+      </div>
+      <div className="content">
+        <h3 className="header">{hog.name}</h3>
+      </div>
+      {showDetails && (
+        <div className="extra content">
+          <p>Specialty: {hog.specialty}</p>
+          <p>Weight: {hog.weight}</p>
+          <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+          <p>Highest Medal: {hog["highest medal achieved"]}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default HogCard;
